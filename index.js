a9f482608026feee15f1dd0982f84c8557dcd056
Lianne Turnbull <lianne.turnbull@gmail.com>
2022-10-14 16:38:26 +0100

    handle file upload

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 var express = require("express");
 var cors = require("cors");
+const multer = require("multer");
 require("dotenv").config();
 
 const app = express();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function (req, res) {
     res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), async (req, res) => {
+    const file = req.file;
+    res.json({
+        name: file.originalname,
+        type: file.mimetype,
+        size: file.size,
+    });
 });
 
 const port = process.env.PORT || 3000;
