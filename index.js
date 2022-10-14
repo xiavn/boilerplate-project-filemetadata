55c63cc848b6f3df23af81380a8b786a580c3183
Lianne Turnbull <lianne.turnbull@gmail.com>
2022-10-14 16:01:19 +0100

    Add mongoose declaration

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 
-var app = express();
+const mongoose = require("mongoose");
+const app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
 
-
+app.get("/", function (req, res) {
+    res.sendFile(process.cwd() + "/views/index.html");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log("Your app is listening on port " + port);
 });
